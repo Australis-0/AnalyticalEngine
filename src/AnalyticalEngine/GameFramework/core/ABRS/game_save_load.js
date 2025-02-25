2ae277ed04ea4aa0666cdf4d010aa9e72f0e0107
Confoederatio <htmlprogrammingbn@gmail.com>
2025-02-25 20:12:11 +0100

    updated .jar buildd

--- src/AnalyticalEngine/GameFramework/core/ABRS/game_save_load.js
+++ src/AnalyticalEngine/GameFramework/core/ABRS/game_save_load.js
@@ -1,22 +1,6 @@
 //Initialise functions
 {
 	function initialiseGameSaveLoad () {
-		//Game Save handler
-		onGameSave(function (arg0_save_key) {
-			//Convert from parameters
-			var save_key = arg0_save_key;
-
-			//Declare local instance variables
-			var save_file_path = "saves/" + Game.map.getFile_ActiveMap_Path() + save_key + "/AnalyticalEngine.js";
-
-			//Save main.gamestate
-			try {
-				writeObjectToFile(main.gamestate, save_file_path);
-			} catch (e) {
-				console.warn(e.stack);
-			}
-		});
-
 		//Game Load handler
 		onGameLoad(function (arg0_load_key) {
 			//Convert from parameters
@@ -28,6 +12,22 @@
 			//Load main.gamestate
 			try {
 				main.gamestate = loadFileAsObject(load_file_path);
+			} catch (e) {
+				console.warn(e.stack);
+			}
+		});
+
+		//Game Save handler
+		onGameSave(function (arg0_save_key) {
+			//Convert from parameters
+			var save_key = arg0_save_key;
+
+			//Declare local instance variables
+			var save_file_path = "saves/" + Game.map.getFile_ActiveMap_Path() + save_key + "/AnalyticalEngine.js";
+
+			//Save main.gamestate
+			try {
+				writeObjectToFile(main.gamestate, save_file_path);
 			} catch (e) {
 				console.warn(e.stack);
 			}
