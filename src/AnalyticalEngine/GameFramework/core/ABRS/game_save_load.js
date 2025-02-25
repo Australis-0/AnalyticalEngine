7d6b0dec26c0039713c7e92e5475dc3271e09861
Confoederatio <htmlprogrammingbn@gmail.com>
2025-02-25 20:42:15 +0100

    should be fixed

--- src/AnalyticalEngine/GameFramework/core/ABRS/game_save_load.js
+++ src/AnalyticalEngine/GameFramework/core/ABRS/game_save_load.js
@@ -1,22 +1,6 @@
 //Initialise functions
 {
 	function initialiseGameSaveLoad () {
-		//Game Load handler
-		onGameLoad(function (arg0_load_key) {
-			//Convert from parameters
-			var load_key = arg0_load_key;
-
-			//Declare local instance variables
-			var load_file_path = "saves/" + Game.map.getFile_ActiveMap_Path() + load_key + "/AnalyticalEngine.js";
-
-			//Load main.gamestate
-			try {
-				main.gamestate = loadFileAsObject(load_file_path);
-			} catch (e) {
-				console.warn(e.stack);
-			}
-		});
-
 		//Game Save handler
 		onGameSave(function (arg0_save_key) {
 			//Convert from parameters
@@ -28,6 +12,22 @@
 			//Save main.gamestate
 			try {
 				writeObjectToFile(main.gamestate, save_file_path);
+			} catch (e) {
+				console.warn(e.stack);
+			}
+		});
+
+		//Game Load handler
+		onGameLoad(function (arg0_load_key) {
+			//Convert from parameters
+			var load_key = arg0_load_key;
+
+			//Declare local instance variables
+			var load_file_path = "saves/" + Game.map.getFile_ActiveMap_Path() + load_key + "/AnalyticalEngine.js";
+
+			//Load main.gamestate
+			try {
+				main.gamestate = loadFileAsObject(load_file_path);
 			} catch (e) {
 				console.warn(e.stack);
 			}
